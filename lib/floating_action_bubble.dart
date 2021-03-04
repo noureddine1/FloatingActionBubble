fec81f4505ae66b57b13ce36d9c27a868bde5784
Noureddine Debiane <nordindebiane@gmail.com>
2021-03-04 08:30:56 +0100

    increased width

--- lib/floating_action_bubble.dart
+++ lib/floating_action_bubble.dart
@@ -37,7 +37,7 @@ class BubbleMenu extends StatelessWidget {
       onPressed: item.onPress,
       child: Container(
         height: MediaQuery.of(context).size.height * 0.07,
-        width: MediaQuery.of(context).size.width * 0.3,
+        width: MediaQuery.of(context).size.width * 0.45,
         child: Directionality(
           textDirection: TextDirection.rtl,
           child: Row(
